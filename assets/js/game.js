99bde5cd7aa5aa506caa5227c3ffacf60ef40ac6
DJS <davidstahl@Davids-MacBook-Pro.local>
2020-03-24 07:21:23 -0700

    initial file structure

---
+++ assets/js/game.js
@@ -0,0 +1,18 @@
+
+// communicating with the user
+
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+// this creates a function name "figt"
+
+function fight() {
+    window.alert("The figh has begun!");
+
+}
+
+// fight();
