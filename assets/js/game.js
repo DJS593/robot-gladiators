4759150fc9dfc71b1f45a84bff8118402bec5b1e
DJS <davidstahl@Davids-MacBook-Pro.local>
2020-03-24 10:22:29 -0700

    fight variable created for game

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,18 +1,42 @@
-
-// communicating with the user
-
 var playerName = window.prompt("What is your robot's name?");
-console.log(playerName);
-// this will do math and log 20
-console.log(10 + 10);
-// what is this?
-console.log("Our robot's name is " + playerName);
+var playerHealth = 100;
+var playerAttack = 10;
 
-// this creates a function name "figt"
+console.log(playerName, playerAttack, playerHealth);
 
-function fight() {
-    window.alert("The figh has begun!");
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-}
+var fight = function() {
+  window.alert("Welcome to Robot Gladiators!");
+};
 
-// fight();
+enemyHealth = enemyHealth - playerAttack;
+
+console.log(
+    playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+);
+
+    if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died!");
+    } 
+    else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+
+playerHealth = playerHealth - enemyAttack;
+
+console.log(
+    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+);
+
+    if (playerHealth <= 0) {
+    window.alert(playerName + " has died!");
+    } 
+    else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
+
+
+fight();
